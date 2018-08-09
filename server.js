ff9f4b59723742ae5820ff1e3edb6a108a86f47b
NI GUO <guoni0909@gmail.com>
2018-08-09 16:01:27 -0700

    updated server, client folers

--- server.js
+++ server.js
@@ -1,16 +1,28 @@
 const express = require('express');
-const fs = require('fs');
 const bodyParser = require('body-parser');
+const fs = require('fs');
+const db = JSON.parse(fs.readFileSync('data/data.json', 'utf8'));
 
-const add = express();
-const http = require('http').Server(app);
+let app = express();
+let http = require('http').Server(app);
 
 app.use(express.static('client/'));
+//app.use('/profile', express.static('edit-profile/')); when users go to URL/profile send them to "edit-profile"
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 let PORT = process.env.PORT || 8000;
 
-http.listen(PORT, ()=>{
-    console.log('this server is listening on PORT: ', PORT);
+http.listen(PORT, function(){
+    console.log('this server is listening on PORT: '+ PORT );
+});
+
+// console.log(db.notes);
+
+app.get('/get-data', (req, res)=>{
+    let dataPackage = {
+        "notes": db.notes
+    } 
+    console.log(dataPackage);
+    res.send(dataPackage);
 });
