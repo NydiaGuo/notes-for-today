6b429aa7109b75809b792418cf39daa0440072e6
NI GUO <guoni0909@gmail.com>
2018-08-16 22:39:42 -0700

    grabbing data from database

--- client/js/script.js
+++ client/js/script.js
@@ -1,32 +1,80 @@
-$(document).ready(()=>{
-    let cards = '<div class="card text-white bg-warning mb-3" style="max-width: 18em;"><div class="card-body">';
-    let btns = '<div class="row justify-content-center mt-4"><button class="btn btn-outline-danger mr-2">Update</button><button class="btn btn-outline-danger ml-2">Delete</button></div></div></div>';
-           
-  $.get('http://localhost:8000/get-data', (data)=>{
+$(document).ready(function(){
+  let updateBtn = "<button class='update'>Update</button>";
+  let remove = "<button class='remove'>x</button>";
 
-    let dataArray = data.notes;
+  $.get('http://localhost:8000/get-data', function(data) {
 
-    console.log("this is data array: ", dataArray);
+    console.log("this is get route data: ", data);
 
-    for(let i =0; i< dataArray.length; i++) {
 
-        let text = '<p>'+dataArray[i]+'</p>';
+    // let dataID = data.map(function(list){
+    //   return list._id
+    // })
 
-        $('.cards').append(cards+text+btns);
+    for (let i =0; i < data.length; i++) {
+      // console.log("data of i: ", data[i].$oid);
+      let listItem = "<li data-id="+ dataID+"><a href=#>"+ remove +"<p>" + data[i].text  +"</p></a>"+ updateBtn + "</li>" ;
+      // $("li").attr(data[i].$oid);
+      $("#notes-list").append(listItem);
 
     }
 
+
+    $('.remove').click((e)=>{
+      e.preventDefault();
+
+  
+      console.log("remove clicked: ", $(this).parent());
+  
+      // $.post("http://localhost:8000/drop-data",)
+  
+    });
+
+    $('.update').click((e)=>{
+      e.preventDefault();
+      
+    });
+
+
   });
 
-  $('#add-note').click((e)=>{
+
+  $(document).on('click', '#submit-note', function(e){
+
     e.preventDefault();
-    
-    let newText = $('#note-texts').val();
 
-    let newNote = {
-        "notes": newText
+    let newNote = $("#new-note").val();
+
+    // console.log("this is new note: ", newNote);
+
+    if (newNote !== "") {
+
+      let listItem = "<li><a href=#>"+ remove +"<p>" + newNote  +"</p></a>"+ updateBtn + "</li>" ;
+      
+      let noteContent = {
+        
+        "task": newNote
+      }
+
+      console.log("note content: ", noteContent)
+
+      $.post("http://localhost:8000/set-data", noteContent, function(data){
+
+        console.log("this is new Note: ", data);
+
+      });
+
+      $("#notes-list").append(listItem);
+
+      //empty the textares after clicking
+			$("#new-note").val("");
+    
     }
-    console.log(newNote);
+
   });
+
+
+
+
 
 });
--- server.js
+++ server.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fs = require('fs');
-const db = JSON.parse(fs.readFileSync('data/data.json', 'utf8'));
+// const fs = require('fs');
+const mongoose = require('mongoose');
+// const db = JSON.parse(fs.readFileSync('data/data.json', 'utf8'));
+
+
+let dataURL = 'mongodb://firstdatabase:test2018@ds119692.mlab.com:19692/text2018';
+//connect to mLab
+mongoose.connect(dataURL, function(err){
+    if (err){
+        console.log("errs");
+    } else {
+        console.log("no errs and connected to your mongodb database");
+    }
+    
+});
+
+
+let Note = mongoose.Schema({
+    "text":String
+});
+
+let TheNote = mongoose.model('Notes', Note);
+
 
 let app = express();
 let http = require('http').Server(app);
@@ -17,13 +38,60 @@ http.listen(PORT, function(){
     console.log('this server is listening on PORT: '+ PORT );
 });
 
-// console.log(db.notes);
 
-app.get('/get-data', (req, res)=>{
-    let dataPackage = {
-        "notes": db.notes
-    } 
+
+
+//getting data from database which is the JSON object
+app.get('/get-data', (req,res)=>{
+ 
+    TheNote.find({}).then(function(data){
+        res.send(data);
+        console.log("this is the data array: ", data);
+    });
+
+});
+
+
+app.post('/set-data', (req, res)=>{
+
+    let newNote = new TheNote({
+        "text": req.body.task
+    });
     
-    console.log(dataPackage);
-    res.send(dataPackage);
+    newNote.save(function(err, result){
+        if(err){
+            console.log("saving errs: ",err);
+            return;
+        } else {
+            console.log("this is result: ", result);
+        }
+    
+    
+    });
+
+});
+
+
+app.post('drop-data',(req, res)=>{
+
+    let taskID = req.body._id;
+
+    TheNote.findByIdAndRemove(taskID, function(err, result){
+        if (err) {
+            console.log(err);
+        } else {
+            // let index = -1;
+            console.log("this is the drop-data: ", result);
+            // for (let i=0; i<result.length; i++){
+                // if (result[i]._id.$oid == taskID) {
+                //     index = i;
+                //     break;
+                // }
+            // }
+        }
+        // if(index)
+
+    });
+
+
 });
